3d8bca25365e09db6715f2a08edf79fca9c3c6fa
Regis Santos <regis42santos@gmail.com>
2022-01-09 21:55:58 -0300

    Bugfix: corrige falta do usuário logado.

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,14 @@
 from django.views.generic import TemplateView
 from users.models import User
 
+
 class HomePageView(TemplateView):
     template_name = "home.html"
 
     def get_context_data(self, **kwargs):
         # recupera o contexto da página
         context = super().get_context_data(**kwargs)
-        context['empresa_logada'] = User.objects.get(email=self.request.user).empresa
+        user = User.objects.filter(email=self.request.user).first()
+        if user:
+            context['empresa_logada'] = user.empresa
         return context
