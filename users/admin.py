05229eb9219bdb2626c0d997c4316a78c837a0b8
Regis Santos <regis42santos@gmail.com>
2022-01-09 21:37:38 -0300

    Arruma admin.py

--- users/admin.py
+++ users/admin.py
@@ -4,6 +4,7 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .forms import UserChangeForm, UserCreationForm
 from .models import User, GrupoEmpresarial, Empresas
 
+
 class UserAdmin(BaseUserAdmin):
     def get_form(self, request, obj=None, **kwargs):
         form = super().get_form(request, obj, **kwargs)
@@ -19,9 +20,9 @@ class UserAdmin(BaseUserAdmin):
                 'password2',
                 'sobrenome',
                 'is_active',
-                'is_staff', 
+                'is_staff',
                 'is_superuser',
-                'groups', 
+                'groups',
                 'user_permissions',
             }
         for f in disabled_fields:
@@ -33,19 +34,19 @@ class UserAdmin(BaseUserAdmin):
         if request.user.is_superuser:
             return self.superuser_fieldsets
         else:
-            return self.staff_fieldsets 
+            return self.staff_fieldsets
 
-#Campos do Super-Admin
+# Campos do Super-Admin
     superuser_fieldsets = (
-        (None, {'fields': ('email', 'password', 'nome','sobrenome','last_login')}),
+        (None, {'fields': ('email', 'password', 'nome', 'sobrenome', 'last_login')}),
         ('Permissions', {'fields': (
-            'is_active', 
-            'is_staff', 
+            'is_active',
+            'is_staff',
             'is_superuser',
-            'groups', 
+            'groups',
             'user_permissions',
         )}),
-         ('Configurações Empresa', {'fields': ('empresa','grupo_empresarial')}),
+        ('Configurações Empresa', {'fields': ('empresa', 'grupo_empresarial')}),
     )
     add_fieldsets = (
         (
@@ -57,19 +58,24 @@ class UserAdmin(BaseUserAdmin):
         ),
     )
 
-#Campos dos admin
+# Campos dos admin
     staff_fieldsets = (
-        (None, {'fields': ('email', 'nome','sobrenome','last_login')}),
-         ('Configurações Empresa', {'fields': ('empresa','cargo')}),
+        (None, {'fields': ('email', 'nome', 'sobrenome', 'last_login')}),
+        ('Configurações Empresa', {'fields': ('empresa', 'cargo')}),
     )
 
-
-    list_display = ('email','nome', 'is_active', 'is_staff', 'last_login', 'empresa')
+    list_display = ('email', 'nome', 'is_active', 'is_staff', 'last_login', 'empresa')
     list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups', 'empresa')
     search_fields = ('email',)
     ordering = ('email',)
     filter_horizontal = ('groups', 'user_permissions',)
 
+
+@admin.register(Empresas)
+class EmpresasAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'grupo_empresarial')
+    search_fields = ('razao_social',)
+
+
 admin.site.register(GrupoEmpresarial)
-admin.site.register(Empresas)
 admin.site.register(User, UserAdmin)
